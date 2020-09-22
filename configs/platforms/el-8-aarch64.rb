d91fb615afc54b3a56dedc5e5ceeeaf00b79b389
Ciprian Badescu <ciprian.badescu@puppet.com>
2020-09-22 10:12:36 +0000

    (maint) add support for el-8-aarch64

---
+++ configs/platforms/el-8-aarch64.rb
@@ -0,0 +1,10 @@
+platform 'el-8-aarch64' do |plat|
+  plat.servicedir '/usr/lib/systemd/system'
+  plat.defaultdir '/etc/sysconfig'
+  plat.servicetype 'systemd'
+
+  packages = %w[make cmake gcc-c++]
+  plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
+  plat.install_build_dependencies_with 'dnf install -y --allowerasing'
+  plat.vmpooler_template 'redhat-8-arm64'
+end
