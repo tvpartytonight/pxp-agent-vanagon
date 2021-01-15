4286b902ead91608a8088d66f0827ee4675a14c2
Gabriel Nagy <gabriel.nagy@puppet.com>
2021-01-15 15:52:49 +0200

    (PA-3533) Add support for el-8-ppc64le

---
+++ configs/platforms/el-8-ppc64le.rb
@@ -0,0 +1,10 @@
+platform 'el-8-ppc64le' do |plat|
+  plat.servicedir '/usr/lib/systemd/system'
+  plat.defaultdir '/etc/sysconfig'
+  plat.servicetype 'systemd'
+
+  packages = %w[make cmake gcc-c++]
+  plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
+  plat.install_build_dependencies_with 'dnf install -y --allowerasing'
+  plat.vmpooler_template 'redhat-8-power9'
+end
