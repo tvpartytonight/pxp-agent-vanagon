53f040f40a8ae8c481a73be449053b43ffaf4b03
gimmy <gheorghe.popescu@puppet.com>
2021-04-16 17:02:20 +0300

    (VANAGON-85) remove duplicate systemd on debian 9
    Vanagon already defines systemd in the default
    platform definition. Remove it from here to avoid
    duplication.

--- configs/platforms/debian-9-i386.rb
+++ configs/platforms/debian-9-i386.rb
@@ -1,6 +1,5 @@
 platform "debian-9-i386" do |plat|
   plat.inherit_from_default
 
-  plat.servicetype 'systemd', servicedir: '/lib/systemd/system'
   plat.servicetype 'sysv', servicedir: '/etc/init.d'
 end
