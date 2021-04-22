e45e8f7eddc045e6a29bb0014ccafda11b80f532
Ciprian Badescu <ciprian.badescu@puppet.com>
2021-04-22 15:54:19 +0300

    (PA-3535) update nspooler template name

--- configs/platforms/el-8-ppc64le.rb
+++ configs/platforms/el-8-ppc64le.rb
@@ -6,5 +6,5 @@ platform 'el-8-ppc64le' do |plat|
   packages = %w[make cmake gcc-c++]
   plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
   plat.install_build_dependencies_with 'dnf install -y --allowerasing'
-  plat.vmpooler_template 'redhat-8-power9'
+  plat.vmpooler_template 'redhat-8-power8'
 end
