41b838c412838ffaea7cff726fd0240595f6496e
Ciprian Badescu <ciprian.badescu@puppet.com>
2021-05-20 08:31:30 +0300

    (maint) pin cmake to 3.11.4

--- configs/platforms/el-8-ppc64le.rb
+++ configs/platforms/el-8-ppc64le.rb
@@ -3,7 +3,7 @@ platform 'el-8-ppc64le' do |plat|
   plat.defaultdir '/etc/sysconfig'
   plat.servicetype 'systemd'
 
-  packages = %w[make cmake gcc-c++]
+  packages = %w[make cmake-3.11.4 gcc-c++]
   plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
   plat.install_build_dependencies_with 'dnf install -y --allowerasing'
   plat.vmpooler_template 'redhat-8-power8'
