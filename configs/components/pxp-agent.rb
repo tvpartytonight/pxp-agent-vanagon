afbb6f550a6816b4cc69b205461b70cc5fd88c3a
Christopher Thorn <christopher.thorn@puppet.com>
2022-11-21 11:08:41 -0800

    (maint) Solaris-10 non sparc fix

--- configs/components/pxp-agent.rb
+++ configs/components/pxp-agent.rb
@@ -47,8 +47,12 @@ component 'pxp-agent' do |pkg, settings, platform|
     # PCP-87: If we build with -O3, solaris segfaults due to something in std::vector
     special_flags += " -DCMAKE_CXX_FLAGS_RELEASE='-O2 -DNDEBUG' "
 
-    special_flags += " -DCMAKE_EXE_LINKER_FLAGS=' /opt/puppetlabs/puppet/lib/libssl.so /opt/puppetlabs/puppet/lib/libgcc_s.so /opt/puppetlabs/puppet/lib/libcrypto.so' " if platform.name =~ /^solaris-10/
-    special_flags += " -DCMAKE_EXE_LINKER_FLAGS=' /opt/puppetlabs/puppet/lib/libssl.so /opt/puppetlabs/puppet/lib/libcrypto.so' " if platform.name =~ /^solaris-11/
+    special_flags += if platform.name =~ /^solaris-10-sparc/
+                       " -DCMAKE_EXE_LINKER_FLAGS=' /opt/puppetlabs/puppet/lib/libssl.so /opt/puppetlabs/puppet/lib/libgcc_s.so /opt/puppetlabs/puppet/lib/libcrypto.so' "
+                     else
+                       " -DCMAKE_EXE_LINKER_FLAGS=' /opt/puppetlabs/puppet/lib/libssl.so /opt/puppetlabs/puppet/lib/libcrypto.so' "
+                     end
+
   elsif platform.is_windows?
     make = "#{settings[:gcc_bindir]}/mingw32-make"
     pkg.environment 'CYGWIN', settings[:cygwin]
