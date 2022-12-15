e4d4a9befc85a820373392724e5527536f636ab5
Aria Li <aria.li@puppet.com>
2022-12-14 16:46:08 -0800

    (maint) Remove output directory command
    This commit removes the plat.output_dir command from  macOS 11 (x86) and 12 (x86 & arm) platform definitions since pxp-agent-vanagon does not have that file structure, everything is stored under the artifacts directory on builds.delivery.puppetlabs.net

--- configs/platforms/osx-12-x86_64.rb
+++ configs/platforms/osx-12-x86_64.rb
@@ -17,5 +17,4 @@ platform 'osx-12-x86_64' do |plat|
   plat.provision_with 'su test -c \'echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"\''
   plat.provision_with 'sudo chown -R test:admin /Users/test/'
   plat.vmpooler_template 'macos-12-x86_64'
-  plat.output_dir File.join('apple', '12', 'puppet7', 'x86_64')
 end
